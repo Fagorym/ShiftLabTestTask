adfe9f1e31b39337dce0c957ed81cdc4ced2d437
Fagor <oleg-veber@mail.ru>
2023-02-17 13:15:09 +0700

    create good model

--- src/main/java/ru/nsu/shift/lab/shop/entity/Good.java
+++ src/main/java/ru/nsu/shift/lab/shop/entity/Good.java
@@ -1,6 +1,15 @@
 package ru.nsu.shift.lab.shop.entity;
 
-public class Good {
-    private String serialNumber;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "good")
+public class Good {
+    @Id
+    private String serialNumber;
+    private String manufacturer;
+    private Integer price;
+    private Integer count;
 }
