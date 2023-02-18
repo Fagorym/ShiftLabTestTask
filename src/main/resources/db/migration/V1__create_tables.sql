58e5ac748dffcbe467414c9f97335949fa1783a1
Fagor <oleg-veber@mail.ru>
2023-02-18 14:26:24 +0700

    fix migrations

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -2,5 +2,9 @@ create table good(
     serial_number varchar primary key,
     manufacturer varchar,
     price int,
-    count int
+    count int,
+    monitor_size int,
+    shape varchar,
+    diagonal_inch int,
+    size_gb int
 );
